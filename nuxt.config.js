a5bd40e1c89fb62e0609e5916c9d2d012be2e032
yuki yamagata <yshanxing4@gmail.com>
2022-03-29 08:41:31 +0900

    scss　修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '~assets/common.scss'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
