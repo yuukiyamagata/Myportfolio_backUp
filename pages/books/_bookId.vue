a54e32febadfff5efa5a126b7ef1b8d2fcb52401
yuki yamagata <yshanxing4@gmail.com>
2022-04-05 06:26:35 +0900

    UIの修正

---
+++ pages/books/_bookId.vue
@@ -0,0 +1,148 @@
+<template>
+    <div class="book-page-width">
+      <v-container fluid>
+        <v-row>
+          
+        </v-row>
+      </v-container>
+    </div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      image_src: require("@/static/book.png"),
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+.book-page-width {
+  max-width: 900px;
+}
+
+
+
+
+    // <v-img
+    //               :src="image_src"
+    //               flat
+    //               aspect-ratio="16/9"
+    //               min-height="300"
+    //               max-height="360"
+    //               class="white"
+    //               contain
+    //             />
+
+
+.user-info {
+  /* background-color: blue; */
+  padding: 50px;
+  display: flex;
+}
+
+.user-info-name {
+  margin-left: 20px;
+  align-self: center;
+}
+
+.comment-area {
+  max-width: 350px;
+  min-height: 300px;
+  border: 1px solid white;
+
+  p{
+    overflow-wrap: break-word;
+    word-wrap: break-word;
+  }
+}
+
+.book-title p{
+  display: inline-block;
+  padding-bottom: 20px;
+  border-bottom: 1px solid rgb(226, 216, 216);
+}
+
+.link {
+  color: rgba(226, 24, 74, 0.904);
+  text-decoration: none;
+}
+
+</style>
+
+
+
+
+
+
+
+// <v-row fluid class="mt-10 mx-auto d-flex">
+//       <v-col cols="6">
+//         <div class="d-flex flex-column">
+//           <div class="img-content">
+//           </div>
+//             <div class="subtitle-1 text-center pt-4 book-title">
+//               <p>#{{ $route.params.bookId }}番目の本のタイトル</p>
+//             </div>
+//           <div class="text-center mt-3">
+//             <nuxt-link to="/" class="link">
+//               商品購入はこちらから
+//             </nuxt-link>
+//         </div>
+//       </div>
+//     </v-col>
+
+//   <v-divider vertical></v-divider>
+
+//       <v-col cols="5">
+//         <div class="user-info">
+//         <div class="user-info-avatar">
+//           <v-avatar
+//           color="grey darken-1"
+//           size="62"
+//           >
+//             <v-icon dark size="70">
+//             mdi-account-circle
+//             </v-icon>
+//           </v-avatar>
+//         </div>
+//         <div class="user-info-name">
+//           <p class="text--gray">UserName</p>
+//         </div>
+//       </div>
+
+//       <div class="comment-area">
+//           <h3>コメント</h3>
+//           <p>あああああああああああああああああああああああああああ
+//             ああああああああああああああああああああああああああああ
+//           </p>
+//       </div>
+
+//       <div class="btn--group">
+//         <v-btn
+//           class="mx-2"
+//           fab
+//           dark
+//           small
+//           color="pink"
+//         >
+//         <v-icon>mdi-heart</v-icon>
+//         </v-btn>
+
+//         <v-btn
+//         class="ma-2"
+//         text
+//         icon
+//         color="blue lighten-2"
+//         >
+//           <v-icon>mdi-thumb-up</v-icon>
+//         </v-btn>
+//       </div>
+
+
+//     </v-col>
+//   </v-row>
--- pages/myPage/_id.vue
+++ pages/myPage/_id.vue
@@ -2,23 +2,46 @@
   <div class="mx-auto mypage-width mt-12">
     <v-container class="px-12">
       <v-row>
-        <v-col cols="6">
-          <v-row d-flex>
-            <v-col cols="4">
-              <v-avatar
-                color="primary"
-                size="100"
-              >
-              </v-avatar>
-              <p class="pt-4">#userId{{ $route.params.id }}番のUserName</p>
-            </v-col>
-            <v-col cols="6">
-              <p>ここに自己紹介がはいる</p>
-            </v-col>
-          </v-row>
+        <v-col cols="9">
+            <v-row dense>
+              <v-col cols="12">
+              <!-- ログイン者の画像が入る -->
+                <v-row>
+                  <v-col cols="12" class="d-flex">
+                    <v-avatar size="70" color="primary" class="pt-2">
+                      <v-img
+                        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                        height="200px"
+                      ></v-img>
+                    </v-avatar>
+                    <div class="ml-4">
+                      <p class="pt-2">#{{ $route.params.id }}のUserName</p>
+                      <v-img :src="image_src" class="twitter-icon-color" height="30px" width="30px"/>
+                    </div>
+                  </v-col>
+
+                </v-row>
+              </v-col>
+              <v-col cols="12">
+                <v-card elevation="0">
+                  <v-card-text>
+                    I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+                  </v-card-text>
+                </v-card>
+              </v-col>
+
+
+
+            </v-row>
+
         </v-col>
-        <v-col cols="4" class="text-right">
-          <v-btn color="indigo" :to="{path: `/mypage/${$route.params.id}/mypagEdit`}" outlined>編集</v-btn>
+        <v-col cols="2">
+          <v-btn
+            color="indigo white--text"
+            outlined
+            elevation="0"
+            @click="goToEditMypage"
+            >編集</v-btn>
         </v-col>
       </v-row>
     </v-container>
@@ -26,7 +49,7 @@
     <div class="userInfo">
       <v-divider></v-divider>
         <v-tabs>
-          <v-tab>Home</v-tab>
+          <v-tab @click="goToHome">Home</v-tab>
           <v-tab @click="goToLike">私がおすすめした参考書</v-tab>
           <v-tab @click="goToFav">お気に入りした参考書</v-tab>
       </v-tabs>
@@ -39,15 +62,25 @@
   export default {
     data(){
       return {
-        id: this.$route.params.id
+        image_src: require('@/static/TwitterLogo.png')
+      }
+    },
+    computed:{
+      id(){
+        return Number(this.$route.params.id)
       }
     },
     methods:{
+      goToEditMypage(){
+        this.$router.push(`/mypage/${this.id}/mypageEdit`)
+      },
+      goToHome(){
+        this.$router.push(`/mypage/${this.id}`)
+      },
       goToLike(){
       this.$router.push(`/mypage/${this.id}/like`)
-    },
+      },
       goToFav(){
-        console.log('発火')
         this.$router.push(`/mypage/${this.id}/favorite`)
       },
     }
@@ -59,6 +92,11 @@
 .mypage-width {
   max-width: 900px;
 }
+
+.twitter-icon-color {
+  color: rgba(29,161,242);
+}
+
 
 @media (min-width: 900px){
   .userInfo{
